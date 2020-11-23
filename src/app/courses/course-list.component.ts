999c4f8518cdaf73fcad367e06f62a04e93de5c5
Bruno Moreira <brunoracon@gmail.com>
2020-11-23 18:02:31 -0300

    added filter

---
+++ src/app/courses/course-list.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Course } from './course';
+import { CourseService } from './course.service';
+
+@Component({
+  selector: 'app-course-list',
+  templateUrl: './course-list.component.html'
+})
+export class CourseListComponent implements OnInit {
+
+  filteredCourses: Course[] = [];
+
+  _courses: Course[] = [];
+
+  _filterBy!: string;
+
+  constructor(private courseService: CourseService) { }
+
+  ngOnInit(): void {
+    this._courses = this.courseService.retrieveAll();
+    this.filteredCourses = this._courses;
+  }
+
+  set filter(value: string) {
+    this._filterBy = value;
+    // Filtrando _courses e igualando este valor em memória ao filteredCourses
+    this.filteredCourses = this._courses
+      .filter((course: Course) =>
+        course.name.toLowerCase().indexOf(
+          this._filterBy.toLowerCase()) > -1);
+  }
+
+  get filter() {
+    return this._filterBy;
+  }
+
+}
